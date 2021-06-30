76be01fe8770752dcf58230b9e4d4daed85d6e88
Luciano Horta <lucianohortasantos@gmail.com>
2021-06-29 21:02:26 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import TaskList from './components/TaskList';
+
+function App() {
+  return (
+    <div className='task-app'>
+      <TaskList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TaskList.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import TaskForm from './TaskForm';
+import Task from './Task';
+
+function TaskList() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = task => {
+    if (!task.text || /^\s*$/.test(task.text)) {
+      return;
+    }
+
+    const newTasks = [task, ...tasks];
+
+    setTasks(newTasks);
+    console.log(...tasks);
+  };
+
+  const updateTask = (taskId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
+    }
+
+    setTasks(prev => prev.map(item => (item.id === taskId ? newValue : item)));
+  };
+
+  const removeTask = id => {
+    const removedArr = [...tasks].filter(task => task.id !== id);
+
+    setTasks(removedArr);
+  };
+
+  const completeTask = id => {
+    let updatedTask = tasks.map(task => {
+      if (task.id === id) {
+        task.isComplete = !task.isComplete;
+      }
+      return task;
+    });
+    setTasks(updatedTask);
+  };
+
+  return (
+    <>
+      <h1>Lista de Tarefas</h1>
+      <TaskForm onSubmit={addTask} />
+      <Task
+        tasks={tasks}
+        completeTask={completeTask}
+        removeTask={removeTask}
+        updateTask={updateTask}
+      />
+    </>
+  );
+}
+
+export default TaskList;
